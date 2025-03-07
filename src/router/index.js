6d216170b4c529dfba1e28802d33cca1ff7b31ae
Manish Maharjan <manishnewar26@gmail.com>
2025-03-07 18:58:50 +0000

    Initial DefaultTemplate view setup and mouse backdrop added

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import DefaultTemplate from '@/views/DefaultTemplate.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      component: DefaultTemplate,
+    },
   ],
 })
 
---
+++ src/views/DefaultTemplate.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="font-sans relative overflow-x-hidden">
+    <MouseBackdrop></MouseBackdrop>
+  </div>
+</template>
+<script setup></script>
+
+<style scoped></style>
