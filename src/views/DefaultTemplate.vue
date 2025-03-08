ae5e4766ae295f6958ffb6b1b49e64904a3d5a26
Manish Maharjan <manishnewar26@gmail.com>
2025-03-08 18:23:43 +0000

    shallowRef added to avoid unnecessary reactivity issues with floatingIcons

--- src/views/DefaultTemplate.vue
+++ src/views/DefaultTemplate.vue
@@ -9,29 +9,31 @@
 import NavigationBar from '@/components/default/layouts/NavigationBar.vue'
 import TechIcons from '@/components/common/icons/TechIcons.vue'
 
-import { onMounted } from 'vue'
-
+import { markRaw, onMounted, shallowRef } from 'vue'
 import simpleIcons from '@/plugins/simple-icons'
 import { colors, generateRandomValue } from '@/utils/helpers'
 
-const floatingIcons = []
+const floatingIcons = shallowRef([])
 
 // Generate Icons in form of objects with colors
 const generateFloatingIcons = (iconCounts) => {
   const iconKeys = Object.keys(simpleIcons)
-  for (let i = 0; i < iconCounts; i++) {
+  const newIcons = Array.from({ length: iconCounts }, () => {
     const randomIconKey = generateRandomValue(iconKeys)
-    floatingIcons.push({
-      component: simpleIcons[randomIconKey],
-      color: generateRandomValue(colors),
-      x: Math.random() * 100,
-      y: Math.random() * 100,
-      duration: 15 + Math.random() * 30,
-      delay: Math.random() * 5,
-      opacity: 0.1 + Math.random() * 0.3,
-      scale: 0.5 + Math.random() * 1.5,
-    })
-  }
+    floatingIcons.value.push(
+      markRaw({
+        component: markRaw(simpleIcons[randomIconKey]),
+        color: generateRandomValue(colors),
+        x: Math.random() * 100,
+        y: Math.random() * 100,
+        duration: 15 + Math.random() * 30,
+        delay: Math.random() * 5,
+        opacity: 0.1 + Math.random() * 0.3,
+        scale: 0.5 + Math.random() * 1.5,
+      })
+    )
+  })
+  floatingIcons.value = newIcons
 }
 
 onMounted(() => {
