cb996bcbfc7fb75c56b59bcf9a98eab3554dabfb
Manish Maharjan <manishnewar26@gmail.com>
2025-03-08 18:11:40 +0000

    eslint and prettier configured for proper code formattings

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import prettierConfig from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 export default [
   {
@@ -16,4 +18,25 @@ export default [
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
+  prettierConfig,
+  {
+    // Add Prettier plugin rules
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true, // Use single quotes
+          semi: false, // No semicolons
+          trailingComma: 'es5', // Add trailing commas where valid in ES5
+          printWidth: 80, // Wrap code at 80 characters
+          tabWidth: 2, // Use 2 spaces for indentation
+          useTabs: false, // Use spaces instead of tabs
+          endOfLine: 'auto', // Auto-detect line endings
+        },
+      ],
+    },
+  },
 ]
