a6ba9771d122bb46c4695b4e6e121fdc46a9a693
Manish Maharjan <manishnewar26@gmail.com>
2025-03-09 17:19:36 +0000

    two additional experiences added, location separated

--- src/views/DefaultTemplate.vue
+++ src/views/DefaultTemplate.vue
@@ -76,7 +76,8 @@ const workExperiences = [
   {
     id: 1,
     position: 'React Native Developer',
-    company: 'Hylark, London',
+    company: 'Executives Place Limited',
+    location: 'London, UK',
     period: 'Jan 2025 - Present',
     description:
       'Developed highly responsive mobile application. Collaborated with designers and backend developers to deliver high-quality application on time.',
@@ -86,7 +87,8 @@ const workExperiences = [
   {
     id: 2,
     position: 'Software Intern (Advanced Practice)',
-    company: 'Ignis Technologies, Chester',
+    company: 'Ignis Technologies',
+    location: 'Chester, UK',
     period: 'Oct 2023 - Dec 2023',
     description:
       'As a part of my advanced practice for my masters degree i joined Ignis Technologies. During my tenure I had supported integration of two software applications from core PHP to Vue.js, significantly enhancing front-end performance and user experience.',
@@ -104,23 +106,46 @@ const workExperiences = [
   {
     id: 3,
     position: 'Software Engineer',
-    company: 'Cedargate Technologies, Kathmandu',
+    company: 'Cedargate Technologies',
+    location: 'Kathmandu, Nepal',
     period: 'Sept 2020 - Aug 2022',
     description:
-      'Migrated the critical UI components of our operational management software from Angular JS to React JS fixing UI crash issues along with an increased speed on components loading by 75%.' +
-      ' Led Operational Management department by mentoring newcomers and juniors by supporting them with guidelines where necessary and involved in code reviews.',
-    skills: ['HTML/CSS', 'JavaScript', 'React', 'MySQL', 'Java', 'AWS'],
+      'Migrated the critical UI components of our operational management software from Angular JS to React JS fixing UI crash issues along with an increased speed on components loading by 75%.',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'MySQL', 'Java', 'AWS'],
     logo: '/images/logos/cedargate_logo.png',
   },
   {
     id: 4,
     position: 'React Native Developer',
     company: 'Codelio Solutions',
+    location: 'Kathmandu, Nepal',
     period: 'Aug 2019 - Sep 2020',
     description:
       'Collaborated with product management to design, build and test systems covering the overall SDLC phases for the software system from scratch and successfully delivered 5 mobile app projects based on React Native during my tenure.',
-    skills: ['React Native', 'HTML/CSS', 'JavaScript', 'PHP', 'Laravel'],
+    skills: ['React Native', 'HTML', 'CSS', 'JavaScript', 'PHP', 'Laravel'],
     logo: '/images/logos/codelio_logo.png',
+  },
+  {
+    id: 5,
+    position: 'Lab Instructor',
+    company: 'National College of Computer Studies',
+    location: 'Kathmandu, Nepal',
+    period: 'Dec 2018 - Sep 2019',
+    description:
+      'Supported all computer lab operations and evaluated student performances in the lab and on tests.',
+    skills: ['HTML', 'CSS', 'JavaScript', 'PHP', 'Java', 'MySQL', 'Git'],
+    logo: '/images/logos/nccs_logo.png',
+  },
+  {
+    id: 6,
+    position: 'React Native Intern',
+    company: 'Auxfin Development Nepal',
+    location: 'Kathmandu, Nepal',
+    period: 'Jun 2018 - Dec 2018',
+    description:
+      'Learnt how to work on developing mobile applications using React native framework and successfully migrated Laravel web-based application into a React native mobile application',
+    skills: ['React Native', 'HTML', 'CSS', 'JavaScript', 'Laravel'],
+    logo: '/images/logos/nccs_logo.png',
   },
 ];
 onMounted(() => {
