c12e6179688c80cf1f0c56e3fc887135b109190e
Manish Maharjan <manishnewar26@gmail.com>
2025-03-16 09:38:01 +0000

    LottieAnimation added into global components for easier imports

--- src/plugins/global-components.js
+++ src/plugins/global-components.js
@@ -1,9 +1,11 @@
 // Register global components
 
 import MouseBackdrop from '@/components/common/animations/MouseBackdrop.vue';
+import LottieAnimation from '@/components/common/animations/LottieAnimation.vue';
 
 export default {
   install(app) {
     app.component('MouseBackdrop', MouseBackdrop);
+    app.component('LottieAnimation', LottieAnimation);
   },
 };
