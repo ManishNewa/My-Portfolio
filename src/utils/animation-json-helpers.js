c0b9cba24bd033d6684df7093ae729fd3ff37135
Manish Maharjan <manishnewar26@gmail.com>
2025-03-16 09:48:55 +0000

    new animation json added

--- src/utils/animation-json-helpers.js
+++ src/utils/animation-json-helpers.js
@@ -1,4 +1,5 @@
 import waveAnimation from '@/assets/animations/wave.json';
 import girlBlinkingAnimation from '@/assets/animations/girl-blinking.json';
+import ladyWavingAnimation from '@/assets/animations/lady-waving.json';
 
-export { waveAnimation, girlBlinkingAnimation };
+export { waveAnimation, girlBlinkingAnimation, ladyWavingAnimation };
