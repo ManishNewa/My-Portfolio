8e7c0211f987d82b2829e25f8bb3807303ce60b6
Manish Maharjan <manishnewar26@gmail.com>
2025-03-16 09:36:32 +0000

    animations json file added and a json helper created to handle the imports

---
+++ src/utils/animation-json-helpers.js
@@ -0,0 +1,4 @@
+import waveAnimation from '@/assets/animations/wave.json';
+import girlBlinkingAnimation from '@/assets/animations/girl-blinking.json';
+
+export { waveAnimation, girlBlinkingAnimation };
